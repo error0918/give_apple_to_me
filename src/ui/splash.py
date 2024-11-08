8cf16442d943f23edc76794f98d6838fa746f806
jtaeyeon05 <jtaeyeon05@gmail.com>
2024-11-08 17:00:30 +0900

    feat: splash description

--- src/ui/splash.py
+++ src/ui/splash.py
@@ -9,6 +9,7 @@ class Splash(screen.Screen):
     root = None
     title = None
     start_button = None
+    description = None
     deco1 = None
     deco2 = None
 
@@ -32,6 +33,13 @@ class Splash(screen.Screen):
             font=theme.font(size=40),
             command=lambda : controller.change_screen(select1.Select1(root))
         )
+        self.description = tkinter.Label(
+            root,
+            text = "사과해요나한테, 성균관대학교, 2024",
+            background=theme.color_primary,
+            fg=theme.color_on_content,
+            font=theme.font(size=15)
+        )
         self.deco1 = tkinter.Canvas(root, bg=theme.color_primary, highlightthickness=0)
         self.deco1.create_oval(-360, -360, 360, 360, outline=theme.color_secondary, fill=theme.color_secondary)
         self.deco2 = tkinter.Canvas(root, bg=theme.color_primary, highlightthickness=0)
@@ -42,6 +50,7 @@ class Splash(screen.Screen):
         self.root.configure(background=theme.color_primary)
         self.title.place(x=0, y=540-100, width=720, height=200)
         self.start_button.place(x=360-120, y=700, width=240, height=120)
+        self.description.place(x=360-120, y=1040, width=240, height=30)
         self.deco1.place(x=0, y=0, width=360, height=360)
         self.deco2.place(x=530, y=240, width=180, height=180)
 
@@ -50,5 +59,6 @@ class Splash(screen.Screen):
         self.root.configure(background=theme.color_background)
         self.title.place_forget()
         self.start_button.place_forget()
+        self.description.place_forget()
         self.deco1.place_forget()
         self.deco2.place_forget()
